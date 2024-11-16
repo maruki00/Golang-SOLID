22c01eb09a1a55c8354326f465a462d65c5a316e
maruki00 <aitbaha48@gmail.com>
2024-11-16 01:20:24 +0100

    --

---
+++ 1-SRP.go
@@ -0,0 +1,35 @@
+// 1-  The Single Responsibility Principle
+
+// The Single Responsibility Principle states that a class or module should have only one reason to change.
+// In other words, a type should have a single responsibility, making the code easier to understand and maintain.
+
+package main
+
+type Users map[int]*User
+type User struct {
+	Id    int
+	FName string
+	LName string
+}
+
+// Dab Practice
+// Because User has multiple responsibilities getfull name and save it
+func (u *User) GetFullName() string {
+	return u.FName + " " + u.LName
+}
+func (u *User) Save() {
+	Users[u.Id] = u
+}
+
+//Good Practice is to separite these responsibilities
+
+type UserRepository struct {
+}
+
+func (ur *UserRepository) Save(u *User) {
+	Users[u.Id] = u
+}
+
+func main() {
+
+}
